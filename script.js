dee59f768344230f2ffe93039c6c0f6f559974e7
Joycilane Carvalho <joycarvalho59@gmail.com>
2022-09-01 18:58:54 -0300

    pegar ids

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let quizzes
 let objetoCompleto
 
-function obterQuizzesServidor(){
+function obterQuizzesServidor() {
     let promessa = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
     promessa.then(renderizarQuizzes)
 }
 obterQuizzesServidor()
 
-function renderizarQuizzes(resposta){
+function renderizarQuizzes(resposta) {
     console.log(resposta.data)
     quizzes = resposta.data
 
@@ -17,9 +17,9 @@ function renderizarQuizzes(resposta){
 
     let item = document.querySelector('ul')
 
-    for(i=0;i<quizzes.length;i++){
+    for (i = 0; i < quizzes.length; i++) {
 
-        item.innerHTML+= `<li onclick="segundaTela(this)">
+        item.innerHTML += `<li onclick="pegarId(${quizzes[i].id})">
         <div class="tela-preta"></div>
         <div class="card-quizz">
             <img src="${quizzes[i].image}">
@@ -32,7 +32,14 @@ function renderizarQuizzes(resposta){
     }
 }
 
-function segundaTela(){
+function pegarId(id) {
+    console.log(id);
+    const divMain = document.querySelector('.main')
+    divMain.classList.add('esconder')
+    
+}
+
+function segundaTela() {
 
 
     objetoCompleto  //essa variável está com o objeto completinho para você usar e também ja está no onclick, quando clicar em algum quizz, vai executar essa função.
